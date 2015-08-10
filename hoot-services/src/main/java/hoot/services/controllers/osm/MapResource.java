037217ed2a5901a9d539f569904cef4b483aee34
Brandon Witham <brandon.witham@digitalglobe.com>
2015-08-10 13:53:47 -0400

    Revert "Fix some xml injection errors"
    This reverts commit 5aef19395b72d84b9058dc66817003b03907242b.

--- hoot-services/src/main/java/hoot/services/controllers/osm/MapResource.java
+++ hoot-services/src/main/java/hoot/services/controllers/osm/MapResource.java
@@ -36,7 +36,6 @@ import hoot.services.models.osm.Map;
 import hoot.services.models.osm.MapLayers;
 import hoot.services.models.osm.ModelDaoUtils;
 import hoot.services.utils.ResourceErrorHandler;
-import hoot.services.utils.XmlDocumentBuilder;
 import hoot.services.writers.osm.MapQueryResponseWriter;
 
 import java.io.StringWriter;
@@ -271,7 +270,7 @@ public class MapResource
 
     osmElem.appendChild(wayElem);
 
-    Transformer tf = XmlDocumentBuilder.getSecureTransformerFactory().newTransformer();
+    Transformer tf = TransformerFactory.newInstance().newTransformer();
     tf.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
     tf.setOutputProperty(OutputKeys.INDENT, "yes");
     Writer out = new StringWriter();
