7410ec728271337fcbb013ac8ec5c8d6d194d755
Terry Ott <terry.ott@digitalglobe.com>
2016-01-15 12:52:14 -0500

    Adding new install/provision files

---
+++ AWSProvision-Ubuntu1404.sh
@@ -0,0 +1,270 @@
+#!/usr/bin/env bash
+
+# Update OS
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get dist-upgrade -y
+sudo apt-get install ntp -y
+
+# Add the postgres repo
+if [ ! -f /etc/apt/sources.list.d/pgdg.list ]; then
+    echo "Adding PostgreSQL repository to apt"
+    sudo bash -c "echo 'deb http://apt.postgresql.org/pub/repos/apt/ '$(lsb_release -cs)'-pgdg main' > /etc/apt/sources.list.d/pgdg.list"
+    wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+    sudo apt-get update -y
+    sudo apt-get upgrade -y
+fi
+
+# Install dependencies
+echo "Installing dependencies"
+sudo apt-get install -y texinfo g++ libicu-dev libqt4-dev git-core libboost-dev libcppunit-dev libcv-dev libopencv-dev libgdal-dev liblog4cxx10-dev libnewmat10-dev libproj-dev python-dev libjson-spirit-dev automake1.11 protobuf-compiler libprotobuf-dev gdb libqt4-sql-psql libgeos++-dev swig lcov tomcat6 openjdk-7-jdk openjdk-7-dbg maven libstxxl-dev nodejs-dev nodejs-legacy doxygen xsltproc asciidoc pgadmin3 curl npm libxerces-c28 libglpk-dev libboost-all-dev source-highlight texlive-lang-all graphviz w3m python-setuptools python python-pip git ccache libogdi3.2-dev gnuplot python-matplotlib libqt4-sql-sqlite wamerican-insane
+
+if ! dpkg -l | grep --quiet wamerican-insane; then
+    # See /usr/share/doc/dictionaries-common/README.problems for details
+    # http://www.linuxquestions.org/questions/debian-26/dpkg-error-processing-dictionaries-common-4175451951/
+    sudo apt-get install -y wamerican-insane
+    sudo /usr/share/debconf/fix_db.pl
+    sudo dpkg-reconfigure dictionaries-common
+fi
+
+sudo apt-get autoremove -y
+
+# Hoot Baseline is PostgreSQL 9.1 and PostGIS 1.5, so we need a deb file and
+# then remove 9.4
+if [ ! -f postgresql-9.1-postgis_1.5.3-2_amd64.deb ]; then
+    echo "Installing PostgreSQL 9.1"
+    wget http://launchpadlibrarian.net/86690107/postgresql-9.1-postgis_1.5.3-2_amd64.deb
+    sudo dpkg -i postgresql-9.1-postgis_1.5.3-2_amd64.deb
+    sudo apt-get -f install -y
+    # fixes missing dependency of postgis 1.5 by installing postgresql 9.1. 9.1 is installed listening on the default port, 5432. It unfortunately also installs postgres 9.4 but we remove that cleanly in the following steps, while leaving postgres 9.1 untouched
+    echo "Removing PostgreSQL 9.4"
+    sudo apt-get purge -y postgresql-9.4 postgresql-client-9.4 postgresql-9.4-postgis-scripts
+    sudo apt-get install -y postgresql-contrib-9.1
+fi
+
+# Get GDAL & FileGDB_API source
+if [ ! -f gdal-1.10.1.tar.gz ]; then
+    echo "Downloading GDAL source"
+    wget http://download.osgeo.org/gdal/1.10.1/gdal-1.10.1.tar.gz
+fi
+if [ ! -d gdal-1.10.1 ]; then
+    echo "Extracting GDAL source"
+    tar zxvfp gdal-1.10.1.tar.gz
+fi
+if [ ! -f FileGDB_API_1_3-64.tar.gz ]; then
+    echo "Downloading FileGDB API source"
+    wget http://downloads2.esri.com/Software/FileGDB_API_1_3-64.tar.gz
+fi
+if [ ! -d /usr/local/FileGDB_API ]; then
+    echo "Extracting FileGDB API source & installing lib"
+    sudo tar xvfp FileGDB_API_1_3-64.tar.gz --directory /usr/local
+    sudo sh -c "echo '/usr/local/FileGDB_API/lib' > /etc/ld.so.conf.d/filegdb.conf"
+fi
+
+# Build GDAL w/ FileGDB
+if ! ogrinfo --formats | grep --quiet FileGDB; then
+    echo "Building GDAL w/ FileGDB"
+    export PATH=/usr/local/lib:/usr/local/bin:$PATH
+    cd gdal-1.10.1
+    sudo ./configure --with-fgdb=/usr/local/FileGDB_API --with-pg=/usr/bin/pg_config --with-python
+    sudo make -j$(nproc)
+    sudo make install
+    cd swig/python
+    python setup.py build
+    sudo python setup.py install
+    sudo ldconfig
+    cd ~
+fi
+
+# Node.js modules
+if ! grep --quiet NODE_PATH ~/.profile; then
+    echo "Installing node js dependencies"
+    sudo npm config set registry http://registry.npmjs.org/
+    sudo npm install -g xml2js htmlparser imagemagick mocha@1.20.1 express@3.1.2 async html-to-text restler
+    echo 'Adding NODE_PATH to user environment'
+    echo 'export NODE_PATH=/usr/local/lib/node_modules' >> ~/.profile
+    source ~/.profile
+fi
+
+# Module needed for OSM API db test
+if [ ! -d ${HOME}/.cpan ]; then
+    (echo y;echo o conf prerequisites_policy follow;echo o conf commit)|sudo cpan
+    sudo perl -MCPAN -e 'install XML::Simple'
+fi
+
+# Create Services Database
+if ! sudo -u postgres psql -lqt | grep -i --quiet hoot; then
+    echo "Creating Services Database"
+    sudo -u postgres createuser --superuser hoot
+    sudo -u postgres psql -c "alter user hoot with password 'hoottest';"
+    sudo -u postgres createdb hoot --owner=hoot
+    sudo -u postgres createdb wfsstoredb --owner=hoot
+    sudo -u postgres psql -d hoot -c 'create extension hstore;'
+    sudo -u postgres psql -d postgres -c "UPDATE pg_database SET datistemplate='true' WHERE datname='wfsstoredb'"
+    sudo -u postgres psql -d wfsstoredb -f /usr/share/postgresql/9.1/contrib/postgis-1.5/postgis.sql
+    sudo -u postgres psql -d wfsstoredb -f /usr/share/postgresql/9.1/contrib/postgis-1.5/spatial_ref_sys.sql
+    sudo -u postgres psql -d wfsstoredb -c "GRANT ALL on geometry_columns TO PUBLIC;"
+    sudo -u postgres psql -d wfsstoredb -c "GRANT ALL on geography_columns TO PUBLIC;"
+    sudo -u postgres psql -d wfsstoredb -c "GRANT ALL on spatial_ref_sys TO PUBLIC;"
+fi
+
+# PostgreSQL Tuning
+if ! grep -i --quiet HOOT /etc/postgresql/9.1/main/postgresql.conf; then
+echo "Tuning PostgreSQL"
+sudo -u postgres sed -i.bak s/^max_connections/\#max_connections/ /etc/postgresql/9.1/main/postgresql.conf
+sudo -u postgres sed -i.bak s/^shared_buffers/\#shared_buffers/ /etc/postgresql/9.1/main/postgresql.conf
+sudo -u postgres bash -c "cat >> /etc/postgresql/9.1/main/postgresql.conf" <<EOT
+
+#--------------
+# Hoot Settings
+#--------------
+max_connections = 1000
+shared_buffers = 1024MB
+max_files_per_process = 1000
+work_mem = 16MB
+maintenance_work_mem = 256MB
+checkpoint_segments = 20
+autovacuum = off
+EOT
+fi
+
+# Update shared memory limits in OS
+if ! sysctl -e kernel.shmmax | grep --quiet 1173741824; then
+    echo "Setting kernel.shmmax"
+    sudo sysctl -w kernel.shmmax=1173741824
+    sudo sh -c "echo 'kernel.shmmax=1173741824' >> /etc/sysctl.conf"
+fi
+if ! sysctl -e kernel.shmall | grep --quiet 2097152; then
+    echo "Setting kernel.shmall"
+    sudo sysctl -w kernel.shmall=2097152
+    sudo sh -c "echo 'kernel.shmall=2097152' >> /etc/sysctl.conf"
+fi
+
+# Restart PostgreSQL
+sudo service postgresql restart
+
+# Configure and Build
+cd /usr/local/hoot
+source ./SetupEnv.sh
+
+# Check that hoot-ui submodule has been init'd and updated
+if [ ! "$(ls -A hoot-ui)" ]; then
+    echo "hoot-ui is empty"
+    echo "init'ing and updating submodule"
+    git submodule init && git submodule update
+fi
+
+# Configure Tomcat
+if ! grep -i --quiet HOOT /etc/default/tomcat6; then
+echo "Configuring tomcat6 environment"
+sudo bash -c "cat >> /etc/default/tomcat6" <<EOT
+
+#--------------
+# Hoot Settings
+#--------------
+HOOT_HOME=/usr/local/hoot
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib:$HOOT_HOME/lib:$HOOT_HOME/pretty-pipes/lib
+GDAL_DATA=/usr/local/share/gdal
+GDAL_LIB_DIR=/usr/local/lib
+HOOT_WORKING_NAME=hoot
+PATH=$HOOT_HOME/bin:$PATH
+EOT
+fi
+
+# Change Tomcat umask to group write
+if ! grep -i --quiet 'umask 002' /etc/default/tomcat6; then
+echo "Changing Tomcat umask to group write"
+sudo bash -c "cat >> /etc/default/tomcat6" <<EOT
+# Set tomcat6 umask to group write because all files in shared folder are owned by vagrant
+umask 002
+EOT
+fi
+
+# Change Tomcat java opts
+if grep -i --quiet '^JAVA_OPTS=.*\-Xmx128m' /etc/default/tomcat6; then
+    echo "Changing Tomcat java opts"
+    sudo sed -i.bak "s@\-Xmx128m@\-Xms512m \-Xmx2048m \-XX:PermSize=512m \-XX:MaxPermSize=4096m@" /etc/default/tomcat6
+fi
+
+# Fix env var path for GDAL_DATA
+if grep -i --quiet 'gdal/1.10' /etc/default/tomcat6; then
+    echo "Fixing Tomcat GDAL_DATA env var path"
+    sudo sed -i.bak s@^GDAL_DATA=.*@GDAL_DATA=\/usr\/local\/share\/gdal@ /etc/default/tomcat6
+fi
+
+# Remove gdal libs installed by libgdal-dev that interfere with
+# mapedit-export-server using gdal libs compiled from source (fgdb support)
+#if [ -f /usr/lib/libgdal.* ]; then
+    echo "Removing GDAL libs installed by libgdal-dev"
+    sudo rm /usr/lib/libgdal.*
+#fi
+
+# Create Tomcat context path for tile images
+if ! grep -i --quiet 'ingest/processed' /etc/tomcat6/server.xml; then
+    echo "Adding Tomcat context path for tile images"
+    sudo sed -i.bak "s@<\/Host>@  <Context docBase=\"\/usr\/local\/hoot\/ingest\/processed\" path=\"\/static\" \/>\n      &@" /etc/tomcat6/server.xml
+	sudo mkdir -p ${HOOT_HOME}/ingest/processed/
+	sudo chmod -R 777 ${HOOT_HOME}/ingest
+fi
+
+# Allow linking in Tomcat context
+if ! grep -i --quiet 'allowLinking="true"' /etc/tomcat6/context.xml; then
+    echo "Set allowLinking to true in Tomcat context"
+    sudo sed -i.bak "s@^<Context>@<Context allowLinking=\"true\">@" /etc/tomcat6/context.xml
+fi
+
+# Create directory for webapp
+if [ ! -d /usr/share/tomcat6/.deegree ]; then
+    echo "Creating directory for webapp"
+    sudo mkdir /usr/share/tomcat6/.deegree
+    sudo chown tomcat6:tomcat6 /usr/share/tomcat6/.deegree
+fi
+
+# Tweak dev environment to make tests run faster
+if [ ! -f $HOOT_HOME/hoot-services/src/main/resources/conf/local.conf ]; then
+    echo 'testJobStatusPollerTimeout=250' > $HOOT_HOME/hoot-services/src/main/resources/conf/local.conf
+fi
+
+# Update the init.d script for node-mapnik-server
+sudo cp node-mapnik-server/init.d/node-mapnik-server /etc/init.d
+sudo chmod a+x /etc/init.d/node-mapnik-server
+# Make sure all npm modules are installed
+cd node-mapnik-server
+sudo npm install
+cd ..
+
+# Update marker file date now that dependency and config stuff has run
+# The make command will exit and provide a warning to run 'vagrant provision'
+# if the marker file is older than this file (VagrantProvision.sh)
+#touch Vagrant.marker
+
+# Build Hoot
+echo "Configuring Hoot"
+aclocal && autoconf && autoheader && automake && ./configure -q --with-rnd --with-services
+if [ ! -f LocalConfig.pri ] && ! grep --quiet QMAKE_CXX LocalConfig.pri; then
+    echo 'Customizing LocalConfig.pri'
+    cp $HOOT_HOME/LocalConfig.pri.orig $HOOT_HOME/LocalConfig.pri
+    echo 'QMAKE_CXX=ccache g++' >> LocalConfig.pri
+fi
+echo "Building Hoot"
+make clean -sj$(nproc)
+make -sj$(nproc)
+make docs -sj$(nproc)
+
+# Run Tests
+#echo "Running tests"
+#make -sj$(nproc) test
+#make -sj$(nproc) test-all
+
+# Set up Permissions - Added by Mitul
+sudo chmod 777 ${HOOT_HOME}
+
+# Deploy to Tomcat
+echo "Deploying Hoot to Tomcat"
+sudo -u tomcat6 scripts/vagrantDeployTomcat.sh
+
+# Restart Tomcat Server - Added by Mitul
+sudo service tomcat6 restart
+
+# Source Hoot Environments - Added by Mitul
+source ${HOOT_HOME}/SetupEnv.sh
---
+++ docs/install/AWS_Hoot_Install.md
@@ -0,0 +1,53 @@
+﻿# Installing Hootennany on AWS Instance (Ubuntu)
+
+Log into AWS account and launch an AWS Ubuntu 14.04 instance.
+
+MINIMUM AWS INSTANCE SIZE: 
+		m4.xlarge
+RECOMMENDED STORAGE: 50GB
+RECOMMENDED: Assign Public IP to instance
+
+CREATE AWS SECURITY GROUPS to allow these ports for Hootenanny
+		Custom TCP Rule - Port Range: 8096 = P2P Server Port
+		Custom TCP Rule - Port Range: 8080 = Tomcat Web Server Port
+		Custom TCP Rule - Port Range: 8094 = Translation Server Port
+		TCP Rule - Port Range: 22 = SSH into the the AWS Instance
+		
+ITEMS Needed: AWS PEM file to ssh into the AWS instance to install Hootenanny. User can create this before launching the AWS Instance.
+
+# Setting up Hootennany
+
+Once Instance is in "running" state, ssh into instance from command line (putty)
+
+Start by checking out the hoot code, then changing to the branch by typing:
+
+	01) sudo apt-get -y install git
+	02) git clone https://github.com/ngageoint/hootenanny.git hoot​
+        03) sudo mv hoot /usr/local/
+        04) sudo chown ubuntu:ubuntu /usr/local/hoot
+        05) cd /usr/local/hoot
+	06) git checkout <BRANCH_NAME>  #i.e. Replace <BRANCH_NAME> with: "develop", "release", "master" -- additional branches can be found: https://github.com/ngageoint/hootenanny/branches
+	07) git status 		
+        08) git submodule init
+        09) git submodule update
+	10) chmod +x ./AWSProvision-Ubuntu1404.sh
+	11) sudo ./AWSProvision-Ubuntu1404.sh
+	12) source /usr/local/hoot/SetupEnv.sh
+	
+To access the web pages you access the site in your [local Chrome browser](http://localhost:8080/hootenanny-id).
+
+TO UPDATE HOOT CODE:
+If you want the latest hoot code, connect to the AWS Instance via ssh to build and redeploy to Tomcat:
+
+    01) ssh to AWS Instance
+    02) cd /usr/local/hoot
+    03) source SetupEnv.sh
+	04) git status # Check to see which branch is selected
+		05) OPTIONAL: Change branch to build different hoot code: git checkout <BRANCH_NAME>
+	06) git pull
+    07) scripts/ezClean.sh
+	08) cp ${HOOT_HOME}/conf/DatabaseConfig.sh.orig ${HOOT_HOME}/conf/DatabaseConfig.sh
+    09) scripts/ezBuildAll.sh
+    10) sudo -u tomcat6 scripts/vagrantDeployTomcat.sh
+	11) sudo service tomcat6 restart
+	12) source ${HOOT_HOME}/SetupEnv.sh
